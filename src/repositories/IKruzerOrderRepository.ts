df2e8ae54947f5793c54f9e2786e2685a28441db
Patricia Polts <patricia.polts@positiva.eco.br>
2022-02-04 17:21:20 -0300

    limpeza estrutura e atualizado strategia KruzerGetOrderDocument

--- src/repositories/IKruzerOrderRepository.ts
+++
@@ -1,5 +0,0 @@
-import { KruzerOrderStatus } from "@entities/KruzerOrderStatus";
-
-export interface IKruzerOrderRepository { 
-    getByDocument(document: string): Promise<Object>;
-}
--- src/routes.ts
+++ src/routes.ts
@@ -9,9 +9,9 @@ router.all('/', (request, response) => {
   const requestUseCaseController = new RequestUseCaseController();
   return requestUseCaseController.handle(request,response);
 });
-router.all('/users', (request, response) => {
-  return createUserController.handle( request, response );
-});
+// router.all('/users', (request, response) => {
+//   return createUserController.handle( request, response );
+// });
 
 /**
  * APIs  Kruzer
